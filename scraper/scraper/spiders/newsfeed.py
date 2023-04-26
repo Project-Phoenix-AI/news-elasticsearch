89afcae6b62801681c5c5920f1401ee727379ec8
MarcusSamuelsson <Samuelsson.marcus17@gmail.com>
2023-04-26 13:48:19 +0200

    Small change to xpath

--- scraper/scraper/spiders/newsfeed.py
+++ scraper/scraper/spiders/newsfeed.py
@@ -16,13 +16,17 @@ class Spider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        allNews = response.xpath('//div[contains(@class, "module__content")]//ul/li//a[contains(@class, "media__link")]')
-        print("allnews:::: " + allNews.get())
+        allNews = response.xpath('//div[contains(@class, "module__content")]//ul/li//a[contains(@class, "media__link")]|//div[contains(@id, "site-container")]//a[starts-with(@class, "gs-c-promo-heading")]')
+        for news in allNews:
+            print("news:::: " + news.get())
         
         for news in allNews:
+            name = news.xpath('./text()').get().strip()
+            
+
             try:
                 yield {
-                    'name': (news.xpath('./text()').get()).strip(),
+                    'name': news.xpath('/h3/text()|./text()').get().strip(),
                     'link': news.xpath('./@href').get().strip(),
                 }
             except:
