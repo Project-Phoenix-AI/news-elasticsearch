2b8a9c7533ac1cc5d32567160746725ac8ad7b02
MarcusSamuelsson <Samuelsson.marcus17@gmail.com>
2023-04-26 13:11:58 +0200

    fix: Fixed formating for article names and extracted href from each aswell

--- scraper/scraper/spiders/quotes.py
+++ scraper/scraper/spiders/quotes.py
@@ -8,23 +8,25 @@ class Spider(scrapy.Spider):
 
     def start_requests(self):
         urls = [
-            'https://www.bbc.com/'
+            'https://www.bbc.com/',
+            'https://www.bbc.com/news',
+            'https://www.bbc.com/sport'
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        allNews = response.xpath('//section[contains(@class, "module module--promo")]/div/ul/li//a[contains(@class, "media__link")]|//section[contains(@class, "module module--content")]/div/ul/li//a[contains(@class, "media__link")]')
+        allNews = response.xpath('//div[contains(@class, "module__content")]//ul/li//a[contains(@class, "media__link")]')
         print("allnews:::: " + allNews.get())
         
         for news in allNews:
             try:
                 yield {
-                    'name': news.xpath('text()').extract(),
-                    #'link': news.css('a::href').get(),
+                    'name': (news.xpath('./text()').get()).strip(),
+                    'link': news.xpath('./@href').get().strip(),
                 }
             except:
                 yield {
                     'name': "none",
-                    #'link': "none",
+                    'link': "none",
                 }
