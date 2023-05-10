b6da5416af5a00b55c2d8621b37ae62fce8218d0
owwe <berkan-yapici@hotmail.com>
2023-05-11 01:34:40 +0200

    Ranking from feedback is added

--- app.py
+++ app.py
@@ -8,11 +8,13 @@ from RetrieveResults import *
 import requests
 import json
 from elasticsearch_dsl import Search
+from elasticsearch_dsl.query import MultiMatch, Match
 
 app = Flask(__name__)
 #'http://localhost:9200'
 es = Elasticsearch('http://localhost:9200')
-#s = Search(using =es)
+s = Search(using =es,index = 'test-index')
+
 
 
 @app.route('/',methods =["GET","POST"])
@@ -21,17 +23,17 @@ def index():
         q_ = request.form['query']  # Extract the search query from the form data
         if len(q_.split(' ')) >= 0:
             # Result algorithm
-            query = defaultdict(dict)
-            query['multi_match']['query'] = q_
-            query['multi_match']['fields'] = ['name^2','text']
-            query['multi_match']['type'] = 'best_fields'
+            # query = defaultdict(dict)
+            # query['multi_match']['query'] = q_
+            # query['multi_match']['fields'] = ['name^2','text']
+            # query['multi_match']['type'] = 'best_fields'
 
-            # q = Q('bool', should=[
-            #     Q('match', content= q_),
-            #     Q('match', content={'query': q_, 'operator': 'and'}),
-            #     Q('match_phrase', content={'query' : q_,'boost' : 2}),
-            #     Q('multi_match', query= q_, fields=['name', 'text^2'])
-            # ])
+            q = Q('bool', should=[
+                Q('match', content= q_),
+                Q('match', content={'query': q_, 'operator': 'and'}),
+                Q('match_phrase', content={'query' : q_,'boost' : 2}),
+                Q('multi_match', query= q_, fields=['name', 'text^2'])
+                ])
 
             t_initial = time.time()
             resp = es.search(index="test-index", query=query,size = 50)
@@ -49,39 +51,36 @@ def index():
     return render_template('/index.html')
 
 
-
-
-@app.route('/search/<name>', methods=['POST'])
-def search(name):
+@app.route('/search', methods=['POST'])
+def search():
     q_ = request.form['query']  # Extract the search query from the form data
-    if len(q_.split(' ')) >= 0:
-        # Result algorithm
+    s = Search(using = es)
+    if len(q_.split(' ')) > 0:
+        # # Result algorithm
         query = defaultdict(dict)
         query['multi_match']['query'] = q_
         query['multi_match']['fields'] = ['name^2','text']
         query['multi_match']['type'] = 'best_fields'
 
-        # q = Q('bool', should=[
-        #     Q('match', content= q_),
-        #     Q('match', content={'query': q_, 'operator': 'and'}),
-        #     Q('match_phrase', content={'query' : q_,'boost' : 2}),
-        #     Q('multi_match', query= q_, fields=['name', 'text^2'])
-        # ])
 
-        t_initial = time.time()
-        resp = es.search(index="test-index", query=query,size = 50)
-        #resp = s.query(q).extra(size=50).collapse(field='name.raw').execute()
-        #s = Search(using=es, index='my_index').query(q)
-        #s = s.extra(size=1000)  # Increase the result size to 1000 hits
-        #s = s.aggs.bucket('name','',)  # Collapse the results by the 'name' field
-        #s = s.aggs.bucket('name_terms', 'terms', field='name', size=10000)#.metric('top_hits', 'top_hits', size=1)
-        #resp = s.execute()
-        t_final = time.time()
-        number_of_results = resp['hits']['total']['value']
-        results = resp['hits']['hits']
-        took = round((t_final - t_initial),2)
-        return render_template('/search_results.html',results=results, number_of_results = number_of_results,took = took)
-    return render_template('/index.html')
+        #query = defaultdict(dict)
+        #query['bool']['should'] = list({'match': 'name':{'query':q_}},{'match': 'name':{'query':q_,'operator':'and'}}, {'match_phrase': 'name':{'query':q_,'boost':2}})
+        #q = MultiMatch(query=q_, fields=['name', 'text'])
+    else:
+        #q = MultiMatch(query=q_, fields=['name', 'text'])
+        pass
+
+
+    t_initial = time.time()
+    resp = es.search(index="test-index", query=query,size = 50)
+    t_final = time.time()
+    for document in resp['hits']['hits']:
+        document['_score'] *= document['_source']['ranking']
+    number_of_results = resp['hits']['total']['value']
+    results = resp['hits']['hits']
+    took = round((t_final - t_initial),2)
+    return render_template('/search_results.html',results=results, number_of_results = number_of_results,took = took,query = q_.lower())
+#return render_template('/index.html')
 
 @app.route('/update', methods=['POST'])
 def update():
